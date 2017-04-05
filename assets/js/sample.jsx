f8dd2d1a03d0fe9ba0bb20a3929a8b7260691bb6
ochikage <ochi.yoshikage@gmail.com>
2017-04-05 14:23:31 +0900

    popover sample

--- assets/js/sample.jsx
+++ assets/js/sample.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {BrowserRouter, Match, Miss, Link} from 'react-router'
+import Popover from 'react-popover';
 
-var Hello = React.createClass({
-    render: function() {
-        return(<h1>Hello, React!</h1>);
+class PopoverSample extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+        };
     }
-});
+    render() {
+        return(
+            <div>
+                <div>before</div>
+                {this.renderPopover()}
+                <div>after</div>
+            </div>
+        );
+    }
 
-ReactDOM.render(<Hello />, document.getElementById('hello'));
+    renderPopover() {
+        const {show} = this.state;
+        return(
+            <Popover isOpen={show} body={this.renderPopoverBody()} style={{backgroundColor: '#eeeeee'}}>
+                <div style={{width: '100px'}} onClick={()=>{this.setState({show: !this.state.show})}} >popover body</div>
+            </Popover>
+        );
+    }
+
+    renderPopoverBody() {
+        return(
+            <div>
+                <table>
+                    <thead>
+                        <tr><th>Name</th><th>Score</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>Taro</td><td>100</td></tr>
+                        <tr><td>Jiro</td><td>90</td></tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<PopoverSample/>, document.getElementById('hello'));
